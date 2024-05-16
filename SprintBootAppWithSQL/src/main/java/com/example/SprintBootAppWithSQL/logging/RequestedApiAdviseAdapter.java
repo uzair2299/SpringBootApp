3cf1ea178b0342bac90bacfd2ddd45ef6999359d
uzair anwar <uzair.anwar@expertflow.com>
2024-05-16 14:23:11 +0500

    adding looging

---
+++ SprintBootAppWithSQL/src/main/java/com/example/SprintBootAppWithSQL/logging/RequestedApiAdviseAdapter.java
@@ -0,0 +1,2 @@
+package com.example.SprintBootAppWithSQL.logging;public class RequestedApiAdviseAdapter {
+}
---
+++ SprintBootAppWithSQL/src/main/java/com/example/SprintBootAppWithSQL/util/GsonParserUtils.java
@@ -0,0 +1,27 @@
+package za.co.fnb.hybridchat.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import static java.lang.reflect.Modifier.TRANSIENT;
+
+public class GsonParserUtils {
+    private GsonParserUtils() {
+    }
+
+    public static String parseObjectToString(Object object){
+        final Gson gson = new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .excludeFieldsWithModifiers(TRANSIENT)
+                .create();
+        return gson.toJson(object);
+    }
+
+    public static <T> T parseStringToObject(String json, Class<T> classObject){
+        try{
+            return new Gson().fromJson(json, classObject);
+        }catch (Exception e){
+            return null;
+        }
+    }
+}
