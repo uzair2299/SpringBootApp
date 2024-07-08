c69cd27e9a2a68760cba0e46166f3608fddabafc
uzair anwar <uzair.anwar@expertflow.com>
2024-07-08 13:18:00 +0500

    simple socket connection notes

--- SprintBootAppWithSQL/src/main/java/com/example/SprintBootAppWithSQL/controller/SocketConnectionHandler.java
+++ SprintBootAppWithSQL/src/main/java/com/example/SprintBootAppWithSQL/controller/SocketConnectionHandler.java
@@ -1,6 +1,10 @@
 package com.example.SprintBootAppWithSQL.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -11,6 +15,7 @@ import java.util.List;
 
 
 // Socket-Connection Configuration class
+@Slf4j
 public class SocketConnectionHandler extends TextWebSocketHandler {
 
     // In this list all the connections will be stored
@@ -22,14 +27,10 @@ public class SocketConnectionHandler extends TextWebSocketHandler {
     // to the sockets
     @Override
     public void
-    afterConnectionEstablished(WebSocketSession session)
-            throws Exception
-    {
-
+    afterConnectionEstablished(WebSocketSession session) throws Exception {
         super.afterConnectionEstablished(session);
         // Logging the connection ID with Connected Message
-        System.out.println(session.getId() + " Connected uzair");
-
+        log.info(session.getId() + " Connected");
         // Adding the session into the list
         webSocketSessions.add(session);
     }
@@ -37,13 +38,9 @@ public class SocketConnectionHandler extends TextWebSocketHandler {
     // When client disconnect from WebSocket then this
     // method is called
     @Override
-    public void afterConnectionClosed(WebSocketSession session,
-                                      CloseStatus status)throws Exception
-    {
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
-        System.out.println(session.getId()
-                + " DisConnected uzair");
-
+        log.info(session.getId() + " DisConnected");
         // Removing the connection info from the list
         webSocketSessions.remove(session);
     }
@@ -52,24 +49,44 @@ public class SocketConnectionHandler extends TextWebSocketHandler {
     // It will have a session info who is sending the
     // message Also the Message object passes as parameter
     @Override
-    public void handleMessage(WebSocketSession session,
-                              WebSocketMessage<?> message)
-            throws Exception
-    {
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        log.info("Message received at socket");
 
         super.handleMessage(session, message);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //The readTree() method converts a JSON string into a JsonNode tree, which allows you to navigate and manipulate the JSON structure.
+        JsonNode jsonNode = objectMapper.readTree(message.getPayload().toString());
+        String receivedMessage = jsonNode.get("message").asText();
+        System.out.println("Received message: " + receivedMessage);
 
         // Iterate through the list and pass the message to
         // all the sessions Ignore the session in the list
         // which wants to send the message.
-        for (WebSocketSession webSocketSession :
-                webSocketSessions) {
+        for (WebSocketSession webSocketSession : webSocketSessions) {
 //            if (session == webSocketSession)
 //                continue;
 
-            // sendMessage is used to send the message to
-            // the session
-            webSocketSession.sendMessage(message);
+            // sendMessage is used to send the message to the session
+            //Send the JSON string as a TextMessage
+            TextMessage textMessage = new TextMessage(receivedMessage);
+            webSocketSession.sendMessage(textMessage);
+            System.out.println("Send message to the sessions: " + receivedMessage);
         }
     }
 }
+
+//WebSocket is a protocol that provides a full-duplex, bi-directional communication channel over a single, long-lived connection between a client and server.
+// This protocol is designed for real-time web applications, such as chat applications, live notifications, and online gaming.
+//Once a WebSocket connection is established, you cannot send HTTP headers directly through WebSocket frame
+//A WebSocket connection starts with an HTTP GET request but is fundamentally different from a regular HTTP GET call.
+//In a Spring Boot application, the WebSocket port is typically the same as the application's HTTP port.
+//By default, WebSocket endpoints use the same port as the HTTP server.
+//For example, if your Spring Boot application runs on port 8080, the WebSocket server will also be available at ws://localhost:8080/ws-endpoint.
+
+//This is because WebSocket connections are established over the same port as HTTP, which means the WebSocket server runs on the same port as the HTTP server.
+
+//What is the purpose of the WebSocket handshake?
+//Answer:
+//The WebSocket handshake is the initial HTTP request-response process that upgrades an HTTP connection to a WebSocket connection. It involves sending an Upgrade request header from the client and receiving a 101 Switching Protocols response from the server.
+
